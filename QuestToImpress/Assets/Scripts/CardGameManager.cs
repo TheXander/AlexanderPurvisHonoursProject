89a2471f2d2ce82828dad82e384926876f7252ff
TheGamerXander <44140557+TheGamerXander@users.noreply.github.com>
2022-02-14 20:36:21 +0000

    Improvements added card drawing

--- QuestToImpress/Assets/Scripts/CardGameManager.cs
+++ QuestToImpress/Assets/Scripts/CardGameManager.cs
@@ -3,26 +3,141 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CardGameManager : MonoBehaviour
-{ 
+{
     public List<CardInfo> playingCards = new List<CardInfo>();
+    public List<CardInfo> standeredPlayerDeck = new List<CardInfo>();
+    public List<CardInfo> activePlayerDeck = new List<CardInfo>();
+
     public List<GameObject> cardsInHand = new List<GameObject>();
+    public List<GameObject> handSlots = new List<GameObject>();
     public GameObject cardPrefab;
 
-    public List<GameObject> handSlots = new List<GameObject>();
-    int numCardsInHand = 0;
 
     private void Start()
     {
         LoadCSVFile();
+        SetUpPlayerDeck();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {             
+                if (hit.transform.tag == "PlayingCard")
+                {
+                    PlayCard(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    void LoadCSVFile()
+    {
+        // clear list cards of cards
+        playingCards.Clear();
+
+        //Read from CSV file
+        List<Dictionary<string, object>> data = CSVReader.Read("CardInfo");
+        for (var i = 0; i < data.Count; i++)
+        {
+            string nameData = data[i]["Name"].ToString();
+            int cost = int.Parse(data[i]["Cost"].ToString(), System.Globalization.NumberStyles.Integer);
+            int damage = int.Parse(data[i]["Damage"].ToString(), System.Globalization.NumberStyles.Integer);
+            string quote = data[i]["Quote"].ToString();
+
+            CaptureCardFromCSV(nameData, cost, damage, quote);
+        }
+
+        AddCardsToDeck();
+    }
+
+    void CaptureCardFromCSV(string nameData, int cost, int damage, string quote)
+    {
+        CardInfo newCard = new CardInfo(nameData, cost, damage, quote);
+        playingCards.Add(newCard);
+    }
+
+    void AddCardsToDeck()
+    {
+        standeredPlayerDeck = playingCards;
+    }
+
+    void SetUpPlayerDeck() 
+    {
+
+        activePlayerDeck = standeredPlayerDeck;
     }
 
     public void DrawCard()
     {
-        if (numCardsInHand <= 4)
+        if (cardsInHand.Count <= 4)
         {
             GameObject CardInHand = Instantiate(cardPrefab) as GameObject;
-            
-            switch (numCardsInHand)
+
+            SetUpCardTransform(CardInHand);
+
+            AddCardData(CardInHand);
+        }     
+    }
+
+    void AddCardData(GameObject Card) 
+    {
+        CardInfo cardData = playingCards[cardsInHand.Count];
+        Card.GetComponent<PlayingCardControls>().setUpCard(cardData.cardName, cardData.cardCost, cardData.cardDamage, cardData.cardQuote);
+        cardsInHand.Add(Card);
+    }
+
+    void SetUpCardTransform(GameObject Card)
+    {
+        switch (cardsInHand.Count)
+        {
+            case 0:
+                Card.transform.position = handSlots[0].transform.position;
+                Card.transform.rotation = handSlots[0].transform.rotation;
+                break;
+            case 1:
+                Card.transform.position = handSlots[1].transform.position;
+                Card.transform.rotation = handSlots[1].transform.rotation;
+                break;
+            case 2:
+                Card.transform.position = handSlots[2].transform.position;
+                Card.transform.rotation = handSlots[2].transform.rotation;
+                break;
+            case 3:
+                Card.transform.position = handSlots[3].transform.position;
+                Card.transform.rotation = handSlots[3].transform.rotation;
+                break;
+            case 4:
+                Card.transform.position = handSlots[4].transform.position;
+                Card.transform.rotation = handSlots[4].transform.rotation;
+                break;
+            default:
+                print("Error!");
+                break;
+        }
+    }
+
+
+
+    public void PlayCard(GameObject cardToPlay)
+    {
+        cardsInHand.Remove(cardToPlay);
+        Destroy(cardToPlay);
+        Sorthand();
+    }
+
+    void Sorthand()
+    {       
+       for (int i = 0; i < cardsInHand.Count; i++)
+       {
+
+            GameObject CardInHand = cardsInHand[i];
+
+            switch (i)
             {
                 case 0:
                     CardInHand.transform.position = handSlots[0].transform.position;
@@ -44,48 +159,10 @@ public class CardGameManager : MonoBehaviour
                     CardInHand.transform.position = handSlots[4].transform.position;
                     CardInHand.transform.rotation = handSlots[4].transform.rotation;
                     break;
-                    
                 default:
-                    print("Error");
+                    print("Error!");
                     break;
             }
-
-            CardInfo cardData = playingCards[numCardsInHand];
-            CardInHand.GetComponent<PlayingCardControls>().setUpCard(cardData.cardName, cardData.cardCost, cardData.cardDamage, cardData.cardQuote);
-
-            cardsInHand.Add(CardInHand);
-            numCardsInHand++;
-        }     
-    }
-
-    void LoadCSVFile()
-    {
-        // clear list cards of cards
-        playingCards.Clear();
-
-        //Read from CSV file
-        List<Dictionary<string, object>> data = CSVReader.Read("CardInfo");
-        for (var i = 0; i < data.Count; i++)
-        {
-            string nameData = data[i]["Name"].ToString();
-            int cost = int.Parse(data[i]["Cost"].ToString(), System.Globalization.NumberStyles.Integer);
-            int damage = int.Parse(data[i]["Damage"].ToString(), System.Globalization.NumberStyles.Integer);
-            string quote = data[i]["Quote"].ToString();
-
-            CaptureCardFromCSV(nameData, cost, damage, quote);
         }
     }
-
-    void CaptureCardFromCSV(string nameData, int cost, int damage, string quote)
-    {
-        CardInfo newCard = new CardInfo(nameData, cost, damage, quote);
-
-        playingCards.Add(newCard);
-    }
-
-
-    //void Sorthand()
-    //{
-
-    //}
 }
