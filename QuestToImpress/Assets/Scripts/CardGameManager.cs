bb9ab7986120ab6ff8aaec918b954b503bf072dc
TheGamerXander <44140557+TheGamerXander@users.noreply.github.com>
2022-02-14 18:43:48 +0000

    Update to card list

--- QuestToImpress/Assets/Scripts/CardGameManager.cs
+++ QuestToImpress/Assets/Scripts/CardGameManager.cs
@@ -8,14 +8,7 @@ public class CardGameManager : MonoBehaviour
     public List<GameObject> cardsInHand = new List<GameObject>();
     public GameObject cardPrefab;
 
-
-    public GameObject handSlot1;
-    public GameObject handSlot2;
-    public GameObject handSlot3;
-    public GameObject handSlot4;
-    public GameObject handSlot5;
-
-
+    public List<GameObject> handSlots = new List<GameObject>();
     int numCardsInHand = 0;
 
     private void Start()
@@ -31,31 +24,31 @@ public class CardGameManager : MonoBehaviour
             
             switch (numCardsInHand)
             {
-                case 4:
-                    CardInHand.transform.position = handSlot5.transform.position;
-                    CardInHand.transform.rotation = handSlot5.transform.rotation;
-                    break;
-                case 3:
-                    CardInHand.transform.position = handSlot4.transform.position;
-                    CardInHand.transform.rotation = handSlot4.transform.rotation;
-                    break;
-                case 2:
-                    CardInHand.transform.position = handSlot3.transform.position;
-                    CardInHand.transform.rotation = handSlot3.transform.rotation;
+                case 0:
+                    CardInHand.transform.position = handSlots[0].transform.position;
+                    CardInHand.transform.rotation = handSlots[0].transform.rotation;
                     break;
                 case 1:
-                    CardInHand.transform.position = handSlot2.transform.position;
-                    CardInHand.transform.rotation = handSlot2.transform.rotation;
+                    CardInHand.transform.position = handSlots[1].transform.position;
+                    CardInHand.transform.rotation = handSlots[1].transform.rotation;
                     break;
-                case 0:
-                    CardInHand.transform.position = handSlot1.transform.position;
-                    CardInHand.transform.rotation = handSlot1.transform.rotation;
+                case 2:
+                    CardInHand.transform.position = handSlots[2].transform.position;
+                    CardInHand.transform.rotation = handSlots[2].transform.rotation;
                     break;
+                case 3:
+                    CardInHand.transform.position = handSlots[3].transform.position;
+                    CardInHand.transform.rotation = handSlots[3].transform.rotation;
+                    break;
+                case 4:
+                    CardInHand.transform.position = handSlots[4].transform.position;
+                    CardInHand.transform.rotation = handSlots[4].transform.rotation;
+                    break;
+                    
                 default:
                     print("Error");
                     break;
             }
-
 
             CardInfo cardData = playingCards[numCardsInHand];
             CardInHand.GetComponent<PlayingCardControls>().setUpCard(cardData.cardName, cardData.cardCost, cardData.cardDamage, cardData.cardQuote);
@@ -89,4 +82,10 @@ public class CardGameManager : MonoBehaviour
 
         playingCards.Add(newCard);
     }
+
+
+    //void Sorthand()
+    //{
+
+    //}
 }
