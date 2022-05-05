54ab30a7ad4f942f212290b9536b6a539d9dc1ae
TheGamerXander <58525952+TheXander@users.noreply.github.com>
2022-05-06 00:16:48 +0100

    bug fix 3

--- QuestToImpress/Assets/GameFiles/Scripts/DialogScripts/Extras/CastleDialougeTrigger.cs
+++ QuestToImpress/Assets/GameFiles/Scripts/DialogScripts/Extras/CastleDialougeTrigger.cs
@@ -5,14 +5,13 @@ using Yarn.Unity;
 
 public class CastleDialougeTrigger : MonoBehaviour
 {
-    public RomeoData romeoData;
     public DialogueRunner dialogueRunner;
     public string conversationStartNode;
     bool triggered = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {      
-        if (!triggered && romeoData.previousLocation == LevelLoader.Levels.Lake)
+        if (!triggered)
         {
             triggered = true;
             StartConversation();
